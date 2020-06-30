4a4c7e0a805d9f187bccfb9debbc1b9c9c09fbfd
*Althaf <althafmuhammad@computer.org>
2020-06-30 14:47:59 +0530

    Added Display Label

--- Calculator.java
+++ Calculator.java
@@ -1,12 +1,22 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Calculator{
 	public Calculator() {
 		JFrame jf=new JFrame("Calculator");
 		jf.setLayout(null);
 		jf.setSize(600, 600);
+		jf.setLocation(300,100);
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel displayLabel=new JLabel("Output");
+		displayLabel.setBounds(25, 50, 540, 40);
+		displayLabel.setBackground(Color.gray);
+		displayLabel.setOpaque(true);
+		jf.add(displayLabel);
 	}
 	
 	public static void main(String[] args) {
